cb27319d8537b6b498b2e91625be41ffaf473061
bobihariadi <bobihariadi@gmial.com>
2020-09-18 16:41:47 +0700

    first commit

---
+++ _helpers/basic-auth-rest.js
@@ -0,0 +1,39 @@
+
+module.exports = basicAuth;
+function basicAuth(req, res, next) {
+    var auth = req.headers['authorization'];  
+    var Buffer = require('safe-buffer').Buffer
+    var _return = {};
+    if(!auth) {
+        res.statusCode = 401;
+        res.setHeader('WWW-Authenticate', 'Basic realm="Secure Area"');
+        _return = {
+            "status" : "failed",
+            "message": "Need some creds son"
+        };
+        res.end(JSON.stringify(_return));
+    }
+    else if(auth) {
+        var tmp = auth.split(' ');
+        var buf = new Buffer(tmp[1], 'base64');
+        var plain_auth = buf.toString(); 
+        // console.log("Decoded Authorization ", plain_auth);
+
+        var creds = plain_auth.split(':');
+        var username = creds[0];
+        var password = creds[1];
+
+        if((username == 'admin') && (password == '123')) {
+            next();
+        }else {
+            res.statusCode = 401; // Force them to retry authentication
+            res.setHeader('WWW-Authenticate', 'Basic realm="Secure Area"');
+            // res.statusCode = 403;   // or alternatively just reject them altogether with a 403 Forbidden
+            _return = {
+                "status" : "failed",
+                "message": "You shall not pass"
+            };
+            res.end(JSON.stringify(_return));
+        }
+    }
+};
---
+++ index.js
@@ -0,0 +1,76 @@
+
+const mysql = require('mysql');
+const express = require('express');
+var app = express();
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const basicAuth = require('./_helpers/basic-auth-rest');
+
+app.use(bodyparser.json());
+app.use(cors());
+
+app.use(basicAuth); //use basic auth
+
+var mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'db_restci',
+    multipleStatements: true
+});
+
+mysqlConnection.connect((err) => {
+    if (!err)
+        console.log('DB connection succeded');
+    else
+        console.log('DB failed \n Error : ' + JSON.stringify(err, undefined, 2));
+});
+
+
+// get all news
+app.get('/person', (req, res) => {
+    mysqlConnection.query('SELECT * FROM tb_person', (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    });
+});
+
+// get single news
+app.get('/person/:id', (req, res) => {
+    mysqlConnection.query('SELECT * FROM tb_person WHERE ID = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    });
+});
+
+// delete news
+app.delete('/person/:id', (req, res) => {
+    mysqlConnection.query('DELETE FROM tb_person WHERE ID = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send('Delete successfully');
+        else
+            console.log(err);
+    });
+});
+
+// insert news
+app.post('/person', (req, res) => {
+    let emp = req.body;
+
+    var sql = "INSERT INTO tb_person (name, address, phone) VALUES (?,?,?)";
+    mysqlConnection.query(sql, [emp.name, emp.address, emp.phone], (err, rows, fields) => {
+        if (!err) {
+            res.send('New ID : ' + rows.insertId);
+        }
+        else {
+            console.log(err);
+        }
+    });
+});
+
+
+app.listen(3000, () => console.log('Express running on port 3000'));
